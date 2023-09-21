3a4f72395aca117a0167e26b3dd7e96833033dfd
= <=>
2023-09-21 19:01:48 +0530

    login screen design

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,26 @@
+import "./Login.css";
+import logo from "../../images/logo.svg";
+import logotext from "../../images/logotext.svg";
+import handblue from "../../images/hand-blue.svg";
+
+export default function LoginPage() {
+  return (
+    <>
+      <div className="loginbody">
+        <img src={logotext} className="logotext" alt="" />
+        <img src={logo} alt="" />
+        <div className="loginbottom">
+            <div className="campus-text">
+                <p>Campus Ambassador Program</p>
+            </div>
+            <div className="loginfields">
+                <input type="text" placeholder="Username"/>
+                <input type="password" placeholder="Password" />
+                <button><div className="loginbtn"><p>Sign In</p><img src={handblue} alt="" /></div></button>
+            </div>
+            
+        </div>
+      </div>
+    </>
+  );
+}
